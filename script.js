91cf2c315e9a96fcd9e2ff684adb3ba37192f641
Altervl <62514019+Altervl@users.noreply.github.com>
2024-09-02 19:18:35 +0300

    Add main variables, 'operate' function

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+// Main variables
+let firstNum = null;
+let secondNum = null;
+let operator = null;
+
+// Main operations
 function add(a, b) {
     return Number(a) + Number (b);
 };
@@ -12,4 +18,13 @@ function multiply(a, b) {
 
 function divide(a, b) {
     return Number(a) / Number(b);
+};
+
+function operate(a, operator, b) {
+    switch (operator) {
+        case '+': return add(a, b);
+        case '-': return subtract(a, b);
+        case '*': return multiply(a, b);
+        case '/': return divide(a, b);
+    };
 };
